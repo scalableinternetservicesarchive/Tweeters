29d1fa4c4f7eb536faa3763dc36a622a5a7b165e
SatyamA007 <satyamawasthi007@gmail.com>
2021-11-10 21:18:07 -0800

    Added columns in user table and removed profiles table

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :likes
   resources :comments
-  resources :profiles
   resources :followers
   resources :tweets
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'users/sessions'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: redirect('/tweets')
   get "/test", to: "test#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_233920) do
+ActiveRecord::Schema.define(version: 2021_11_11_022507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,18 +42,6 @@ ActiveRecord::Schema.define(version: 2021_11_04_233920) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.text "bio"
-    t.date "joined"
-    t.string "location"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
   create_table "tweets", force: :cascade do |t|
     t.text "content"
     t.integer "likes"
@@ -72,6 +60,10 @@ ActiveRecord::Schema.define(version: 2021_11_04_233920) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "bio"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
