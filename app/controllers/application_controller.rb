a7b7b428910bb5eab051747f8b878aba37c57994
nikoder29 <nikunjbaid@ucsb.edu>
2021-11-24 01:10:15 -0800

    fix CSRF issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception, prepend: true
-
-    before_action :configure_permitted_parameters, if: :devise_controller?
-
-    protected
+    # protect_from_forgery with: :exception, prepend: true
+    #
+    # before_action :configure_permitted_parameters, if: :devise_controller?
+    #
+    # protected
+    skip_forgery_protection
 
          def configure_permitted_parameters
               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
