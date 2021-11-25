927c839a2e1a4e88c22a13a9b144a77cb9090d16
nikoder29 <nikunjbaid@ucsb.edu>
2021-11-25 12:49:04 -0800

    null sign up bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
     # protect_from_forgery with: :exception, prepend: true
     #
-    # before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
     #
-    # protected
     skip_forgery_protection
+    protected
+
 
          def configure_permitted_parameters
               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
