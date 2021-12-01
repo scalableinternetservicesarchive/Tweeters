77e808e7999ef4771539b69763518b61954b7273
samridhimaheshwari <samridhimaheshwari@ucsb.edu>
2021-12-01 15:02:12 -0800

    added pagination

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,7 +8,7 @@ class TweetsController < ApplicationController
     if user_signed_in?
       @users_following = Follower.where(follower_id: current_user.id).to_a.map{|hash| hash.user_id}
       @users_following<<current_user.id
-      @tweets = Tweet.where(users_id: @users_following).paginate(:page => params[:page]).order('id DESC')
+      @tweets = Tweet.where(user_id: @users_following).paginate(:page => params[:page]).order('id DESC')
     else
       @tweets = Tweet.paginate(:page => params[:page]).order('id DESC')
     end
