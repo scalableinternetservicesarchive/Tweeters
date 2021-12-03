f35ff085e64cef6e1ddd8fcdf24c985e3784c26d
gavinoz <91410956+gavinoz@users.noreply.github.com>
2021-12-02 19:56:03 -0800

    Uncommenting tweets seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -164,7 +164,7 @@ end
 #
 for i in 1..no_of_batches do
   User.insert_all(users.slice((i-1)*user_batch_size, user_batch_size))
-  # Tweet.insert_all(tweets.slice((i-1)*tweet_batch_size, tweet_batch_size))
+  Tweet.insert_all(tweets.slice((i-1)*tweet_batch_size, tweet_batch_size))
   Comment.insert_all(comments.slice((i-1)*comment_batch_size, comment_batch_size))
 
   puts "Tweets count : #{Tweet.count} " + DateTime.current.strftime(" on %A, %b. %-d at %-l:%M:%S")
